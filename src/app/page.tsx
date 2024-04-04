52a28b51f4d689e68e1aefa369c1b07cefdab68b
Santiago Crippa <santicrippa@gmail.com>
2024-04-04 20:48:23 -0300

    payment flow completed, adding notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { redirect } from 'next/navigation';
+import { createClient } from '@supabase/supabase-js';
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!)
 
-export default function Home() {
+type Donation = {
+  id: number;
+  created_at: number;
+  amount: number;
+  message: string;
+}[];
+
+export default async function Home() {
+  const donations = await supabase
+    .from('donations')
+    .select('*')
+    .then(({ data }) => data as unknown as Promise<Donation>) // revisar doc supabase para el unknown.
 
   async function donate(formData: FormData) {
     'use server'
@@ -31,19 +52,41 @@ export default function Home() {
   }
 
   return (
-    <>
-      <h1 className="text-2xl mt-5 mb-10">Mercado Pago donations app</h1>
-      <form action={donate} className="grid gap-6 border p-4 rounded-sm">
+    <main className='grid gap-12'>
+      <h1 className="text-2xl mt-5 mb-10 text-center">Donations app with Mercado Pago.</h1>
+
+      <form action={donate} className="grid gap-6 border p-4 rounded-sm max-w-sm mx-auto">
         <Label className="grid gap-2">
           <span>Valor</span>
           <Input type="number" name="amount" />
         </Label>
         <Label className="grid gap-2">
           <span>Tu mensaje en la donacion</span>
-          <Textarea name="message" />
+          <Textarea name="message" className='resize-none' />
         </Label>
-        <Button type="submit">Enviar</Button>
+        <Button className='text-white' type="submit">Enviar</Button>
       </form>
-    </>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Quantity</TableHead>
+            <TableHead className="text-right">Message</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            donations.map((donation) => {
+              return (
+                <TableRow key={donation.id}>
+                  <TableCell className='font-bold'>{donation.amount.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</TableCell>
+                  <TableCell className='text-right'>{donation.message}</TableCell>
+                </TableRow>
+              )
+            })
+          }
+        </TableBody>
+      </Table>
+    </main>
   )
 }
--- src/app/payment/route.ts
+++ src/app/payment/route.ts
@@ -1,12 +1,14 @@
 import { MercadoPagoConfig, Payment } from "mercadopago";
 import { NextRequest } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 
-const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!)
 
 export async function POST(request: NextRequest) {
   const body = await request.json().then(data => data as { data: { id: string } })
 
-  const payment = await new Payment(client).get({ id: body.data.id })
+  const payment = await new Payment(mercadopago).get({ id: body.data.id })
   console.log('payment:', payment);
 
   const donation = {
@@ -14,6 +16,8 @@ export async function POST(request: NextRequest) {
     amount: payment.transaction_amount,
     message: payment.description
   }
+
+  await supabase.from('donations').insert(donation)
 
   return Response.json({ success: true })
 }
